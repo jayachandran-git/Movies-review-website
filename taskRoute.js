0854234c7006d6165f19559d9c7176f0eed3d479
jayachandran-git <148688494+jayachandran-git@users.noreply.github.com>
2024-08-13 10:05:25 +0530

    Delete taskRoute.js

--- taskRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const {createTask} = require("../controllers/taskController");
-
-router.post('/', createTask );
-
-module.exports = router;
